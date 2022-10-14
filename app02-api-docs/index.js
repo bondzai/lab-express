9f759ae86085d171b4fc926fea2710a04b6ae7a9
introbond <introbond.dev@gmail.com>
2022-10-14 15:27:12 +0700

    swagger : post method

--- app02-api-docs/index.js
+++ app02-api-docs/index.js
@@ -7,11 +7,41 @@ const YAML = require('yamljs');
 const swaggerDocument = YAML.load('./swagger.yaml');
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+let courses =[
+    {
+        id: '11',
+        name: 'learn React',
+        price: 299
+    },
+    {
+        id: '22',
+        name: 'learn Angular',
+        price: 399
+    },
+    {
+        id: '33',
+        name: 'learn Django',
+        price: 499
+    }
+];
 
 app.get('/', (req, res) => {
     res.send("hello from server")
 });
+app.get('/api/v1/core', (req, res) => {
+    res.send("hello from server docs")
+});
+app.get('/api/v1/object', (req, res) => {
+    res.send({id: "55", name: "learn Backend", price: 999})
+});
+app.get('/api/v1/courses', (req, res) => {
+    res.send(courses)
+});
+app.get('/api/v1/mycourse/:courseId', (req, res) => {
+    const myCourse = courses.find(course => course.id === req.params.courseId)
+    res.send(myCourse)
+});
 
 app.listen(4000, () =>{
-    console.log(`setver is running on port 4000`)
+    console.log(`sever is running on port 4000`)
 });
