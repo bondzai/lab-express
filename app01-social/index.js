0df2ceeecc67999e30be3a019f7e2f1884955b47
introbond <introbond.dev@gmail.com>
2022-10-14 08:29:31 +0700

    json response

--- app01-social/index.js
+++ app01-social/index.js
@@ -3,7 +3,35 @@ const app = express()
 const port = 4000 || process.env.port
 
 app.get(`/`, (req, res) => {
-    return res.send(`Hello World`)
+    res.status(200).send(`Hello World`)
+})
+
+app.get(`/api/v1/instagram`, (req, res) => {
+    const instaSocial = {
+        username: "jamesbondIG",
+        followers: 77,
+        follows: 10,
+        date: Date.now()
+    }
+    res.status(200).json(instaSocial)
+})
+app.get(`/api/v1/facebook`, (req, res) => {
+    const instaSocial = {
+        username: "jamesbondFB",
+        followers: 77,
+        follows: 10,
+        date: Date.now()
+    }
+    res.status(200).json(instaSocial)
+})
+app.get(`/api/v1/linkedin`, (req, res) => {
+    const instaSocial = {
+        username: "jamesbondLinkedin",
+        followers: 77,
+        follows: 10,
+        date: Date.now()
+    }
+    res.status(200).json(instaSocial)
 })
 
 app.listen(port, () => {
