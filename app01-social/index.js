7285376a85a00307cee45d190608bc1e1aecd64e
introbond <introbond.dev@gmail.com>
2022-10-14 12:00:28 +0700

    working with yaml and swagger

--- app01-social/index.js
+++ app01-social/index.js
@@ -1,12 +1,19 @@
-const express = require(`express`)
-const format = require(`date-format`)
-const app = express()
+const express = require(`express`);
+const format = require(`date-format`);
+const app = express();
+
+// swagger docs related
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const PORT = process.env.PORT || 4000
 
 app.get(`/`, (req, res) => {
     res.status(200).send(`Hello World!`)
-})
-
+});
 app.get(`/api/v1/instagram`, (req, res) => {
     const instaSocial = {
         username: "jamesbondIG",
@@ -15,7 +22,7 @@ app.get(`/api/v1/instagram`, (req, res) => {
         date: format.asString(`dd-MM - hh:mm:ss`, new Date()),
     }
     res.status(200).json(instaSocial)
-})
+});
 app.get(`/api/v1/facebook`, (req, res) => {
     const instaSocial = {
         username: "jamesbondFB",
@@ -24,7 +31,7 @@ app.get(`/api/v1/facebook`, (req, res) => {
         date: format.asString(`dd-MM - hh:mm:ss`, new Date()),
     }
     res.status(200).json(instaSocial)
-})
+});
 app.get(`/api/v1/linkedin`, (req, res) => {
     const instaSocial = {
         username: "jamesbondLinkedin",
@@ -33,12 +40,10 @@ app.get(`/api/v1/linkedin`, (req, res) => {
         date: format.asString(`dd-MM - hh:mm:ss`, new Date()),
     }
     res.status(200).json(instaSocial)
-})
-
+});
 app.get(`/api/v1/:token`, (req, res) => {
     res.status(200).json({ param: req.params.token})
-})
-
+});
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`)
-})
+});
