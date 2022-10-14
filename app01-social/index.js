c78a133230c7b9c2235d798950c2a9286c1a41a2
introbond <introbond.dev@gmail.com>
2022-10-14 07:56:42 +0700

    Hello World

--- app01-social/index.js
+++ app01-social/index.js
@@ -1,1 +1,11 @@
-console.log("Hello")
+const express = require(`express`)
+const app = express()
+const port = 4000 || process.env.port
+
+app.get(`/`, (req, res) => {
+    return res.send(`Hello World`)
+})
+
+app.listen(port, () => {
+    console.log(`server is running at port ${port}`)
+})
