7b0a36a82b53f8cd92cf393f11eaf2965a4d9ae7
introbond <introbond.dev@gmail.com>
2022-10-15 20:06:49 +0700

    learn : token using jsonwebtoken

--- app03-auth/app.js
+++ app03-auth/app.js
@@ -3,7 +3,10 @@ require('./config/database').connect()
 
 const express = require('express')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
 const User = require('./model/user')
+
 const app = express()
 app.use(express.json())
 
@@ -12,26 +15,43 @@ app.get('/', (req, res)=> {
 })
 
 app.get('/register', async (req, res)=> {
-    const {firstname, lastname, email, password} = req.body
+    try {
+        const {firstname, lastname, email, password} = req.body
 
-    if (!(email && password && firstname && lastname)) {
-        res.status(400).send('All fields are required')
+        if (!(email && password && firstname && lastname)) {
+            res.status(400).send('All fields are required')
+        }
+    
+        const existingUser = await User.findOne({ email })
+    
+        if (existingUser) {
+            res.status(401).send('User already exists')
+        }
+    
+        const myEncPassword = await bcrypt.hash(password, 10)
+    
+        const user = await User.create({
+            firstname,
+            lastname,
+            email: email.toLowerCase(),
+            password: myEncPassword
+        })
+        //token
+        const token = jwt.sign(
+            {user_id: user._id, email},
+            process.env.SECRET_KEY,
+            {
+                expiresIn: '2h'
+            }
+        )
+        user.token = token
+        //update or not in DB
+
+        //TODO: hadle password situation
+        res.status(201).json(user)
+    } catch (error) {
+        console.log(error)
     }
-
-    const existingUser = await User.findOne({ email })
-
-    if (existingUser) {
-        res.status(401).send('User already exists')
-    }
-
-    const myEncPassword = await bcrypt.hash(password, 10)
-
-    const user = await User.create({
-        firstname,
-        lastname,
-        email: email.toLowerCase(),
-        password: myEncPassword
-    })
 })
 
 module.exports = app
