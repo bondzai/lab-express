801f9678b1271d97dd48fcfcacd42702ff2e77f2
introbond <introbond.dev@gmail.com>
2022-10-15 10:09:28 +0700

    learn : query params

--- app02-api-docs/index.js
+++ app02-api-docs/index.js
@@ -48,6 +48,11 @@ app.post('/api/v1/addCourse', (req, res) => {
     courses.push(req.body)
     res.send(true)
 })
+app.get('/api/v1/coursequery', (req, res) => {
+    let location = req.query.location
+    let device = req.query.device
+    res.send({ location, device })
+})
 
 app.listen(4000, () =>{
     console.log(`sever is running on port 4000`)
