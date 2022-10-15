9ff8260e84f9dba9045aba78e772ae5b0e5ee6cd
introbond <introbond.dev@gmail.com>
2022-10-15 16:37:43 +0700

    add : process env & mongoose schema

--- app03-auth/index.js
+++ app03-auth/index.js
@@ -1,3 +1,4 @@
 const app = require('./app')
+const {PORT} = process.env
 
-app.listen(4000, () => console.log('server is running at port 4000...'))
+app.listen(PORT, () => console.log(`server is running at port ${PORT}...`))
