965fddd522d5349c007ab0fc6751d7839b8308c5
introbond <introbond.dev@gmail.com>
2022-10-16 07:19:05 +0700

    learn : follow the login flow

--- app03-auth/app.js
+++ app03-auth/app.js
@@ -4,7 +4,6 @@ require('./config/database').connect()
 const express = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-
 const User = require('./model/user')
 
 const app = express()
@@ -17,26 +16,23 @@ app.get('/', (req, res)=> {
 app.post('/register', async (req, res)=> {
     try {
         const {firstname, lastname, email, password} = req.body
-
         if (!(email && password && firstname && lastname)) {
             res.status(400).send('All fields are required')
         }
     
         const existingUser = await User.findOne({ email })
-    
         if (existingUser) {
             res.status(401).send('User already exists')
         }
     
         const myEncPassword = await bcrypt.hash(password, 10)
-    
         const user = await User.create({
             firstname,
             lastname,
             email: email.toLowerCase(),
             password: myEncPassword
         })
-        //token
+
         const token = jwt.sign(
             {user_id: user._id, email},
             process.env.SECRET_KEY,
@@ -45,12 +41,35 @@ app.post('/register', async (req, res)=> {
             }
         )
         user.token = token
-        //update or not in DB
-
-        //handle password situation
         user.password = undefined
 
         res.status(201).json(user)
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+        if (!(email && password)) {
+            res.status(400).send('Filed is missing')
+        }
+        const user = await User.findOne({email})        
+        const DBpassword = await bcrypt.compare(password, user.password)
+        if (user && DBpassword) {
+            const token = jwt.sign(
+                {user_id: user._id, email},
+                process.env.SECRET_KEY,
+                {
+                    expiresIn: '2h'
+                }
+            )
+            user.token = token
+            user.password = undefined
+            res.status(200).json(user)
+        }
+        res.status(400).send('email or password is incorrect')
     } catch (error) {
         console.log(error)
     }
