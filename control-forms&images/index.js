48e6a35d005e8b7488dde53efe0cef8c1871dd76
introbond <introbond.dev@gmail.com>
2022-10-25 15:59:57 +0700

    file length contition merge

--- control-forms&images/index.js
+++ control-forms&images/index.js
@@ -6,78 +6,68 @@ const app = express();
 
 const { CLOUDINARY_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET, PORT } = process.env
 cloudinary.config({
-  cloud_name: CLOUDINARY_NAME,
-  api_key: CLOUDINARY_API_KEY,
-  api_secret: CLOUDINARY_API_SECRET,
+    cloud_name: CLOUDINARY_NAME,
+    api_key: CLOUDINARY_API_KEY,
+    api_secret: CLOUDINARY_API_SECRET,
 });
 
 app.set("view engine", "ejs");
 
-//middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "/tmp/",
-  })
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
 );
 
 app.get("/myget", (req, res) => {
-  console.log(req.body);
-
-  res.send(req.body);
+    res.send(req.body);
 });
 
 app.post("/mypost", async (req, res) => {
-  console.log(req.body);
-  console.log(req.files);
-
-  let result;
-  let imageArray = [];
-
-  // case - multiple images
-
-  if (req.files) {
-    for (let index = 0; index < req.files.samplefile.length; index++) {
-      let result = await cloudinary.uploader.upload(
-        req.files.samplefile[index].tempFilePath,
-        {
-          folder: "users",
+    let result;
+    let imageArray = [];
+    let file = req.files.samplefile;
+    if (req.files.samplefile.length === undefined) {
+        result = await cloudinary.uploader.upload(file.tempFilePath, {
+        folder: "users",
+        });
+        console.log(result);
+        res.send(result);
+    } else if (req.files) {
+        for (let index = 0; index < req.files.samplefile.length; index++) {
+        result = await cloudinary.uploader.upload(
+            req.files.samplefile[index].tempFilePath,
+            {
+                folder: "users",
+            }
+        );
+        imageArray.push({
+            public_id: result.public_id,
+            secure_url: result.secure_url,
+        });
         }
-      );
-
-      imageArray.push({
-        public_id: result.public_id,
-        secure_url: result.secure_url,
-      });
+        details = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            result,
+            imageArray,
+        };
+        console.log(details);
+        res.send(details);
     }
-  }
-
-  // case - single image
-  // let file = req.files.samplefile;
-  // result = await cloudinary.uploader.upload(file.tempFilePath, {
-  //   folder: "users",
-  // });
-
-  console.log(result);
-
-  details = {
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    result,
-    imageArray,
-  };
-  console.log(details);
-
-  res.send(details);
 });
 
+app.get("/", (req, res) => {
+    res.render("home");
+});
 app.get("/mygetform", (req, res) => {
-  res.render("getform");
+    res.render("getform");
 });
 app.get("/mypostform", (req, res) => {
-  res.render("postform");
+    res.render("postform");
 });
 
 app.listen(PORT, () => console.log(`Server is runnning at port ${PORT}`));
